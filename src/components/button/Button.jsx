f825e63f065062ae7bf1ea3eeae95bf2b15cda78
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-08-17 23:50:37 +0200

    Create Button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import style from './Button.module.scss';
+
+const Button = ({ children, type, onClick, variant, size }) => {
+  return (
+    <button
+      className={cn(style[variant], style[size])}
+      type={type}
+      onClick={onClick}
+      variant={variant}
+      size={size}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  type: 'button',
+  children: null,
+  onClick: () => {},
+  variant: 'primary',
+  size: 'md',
+};
+
+Button.propTypes = {
+  variant: PropTypes.oneOf(['primary', 'outline', 'link']),
+  children: PropTypes.node,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  onClick: PropTypes.func,
+  size: PropTypes.oneOf(['md']),
+};
+
+export default Button;
---
+++ src/components/button/Button.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Button from './Button';
+
+describe('Button', () => {
+  it('should output a button', () => {
+    const wrapper = mount(<Button>Title</Button>);
+    expect(wrapper.find('button').length).toEqual(1);
+  });
+
+  it('should set correctly the type when provided', () => {
+    const wrapper = mount(<Button type="submit">Title</Button>);
+    expect(wrapper.getDOMNode().getAttribute('type')).toEqual('submit');
+  });
+
+  it('should call onClick function when the user click', () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(<Button onClick={onClick}>Title</Button>);
+    wrapper.find('button').simulate('click');
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should default to variant="primary and size="md"', () => {
+    const wrapper = mount(<Button>Title</Button>);
+    expect(wrapper.find('button').hasClass('primary')).toEqual(true);
+    expect(wrapper.find('button').hasClass('md')).toEqual(true);
+  });
+
+  it('Should apply variant class', () => {
+    const wrapper = mount(<Button variant="outline">Title</Button>);
+    expect(wrapper.find('button').hasClass('outline')).toEqual(true);
+  });
+});
