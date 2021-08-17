0b0edd96f662fc8de1de6b4a680ed4faa867ab4a
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-08-18 00:15:11 +0200

    Fix outline button style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Button from './components/button/Button';
 function App() {
   return (
     <div>
-      <Button variant="primary">Hello</Button>
+      <Button variant="link">Product &rsaquo;</Button>
     </div>
   );
 }
